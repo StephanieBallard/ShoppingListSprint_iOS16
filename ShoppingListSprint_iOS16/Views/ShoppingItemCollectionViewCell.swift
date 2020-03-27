f2e550c2c6eb93eda8bb7d48ff9cc42496ad0f27
Stephanie Ballard <user.name>
2020-03-27 09:10:30 -0700

    custom classes created and assigned appropriately in the storyboard

---
+++ ShoppingListSprint_iOS16/Views/ShoppingItemCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ShoppingItemCollectionViewCell.swift
+//  ShoppingListSprint_iOS16
+//
+//  Created by Stephanie Ballard on 3/27/20.
+//  Copyright © 2020 Stephanie Ballard. All rights reserved.
+//
+
+import UIKit
+
+class ShoppingItemCollectionViewCell: UICollectionViewCell {
+    
+}
