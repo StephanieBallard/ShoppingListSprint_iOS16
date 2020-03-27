d9ecad7e5bbcc3dd270bc4097c9b06d5ad1d9bc1
Stephanie Ballard <user.name>
2020-03-27 09:19:26 -0700

    safety save, need to close xcode

---
+++ ShoppingListSprint_iOS16/ShoppingItem.swift
@@ -0,0 +1,14 @@
+//
+//  ShoppingItem.swift
+//  ShoppingListSprint_iOS16
+//
+//  Created by Stephanie Ballard on 3/27/20.
+//  Copyright © 2020 Stephanie Ballard. All rights reserved.
+//
+
+import Foundation
+
+struct ShopingItem {
+    var name: String
+    var hasBeenAdded: Bool
+}
